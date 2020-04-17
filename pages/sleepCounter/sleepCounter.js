4d15c54167695a51245219e38d1b521df3444e35
IrvinZhang <245394631@qq.com>
2020-04-17 17:50:43 +0800

    完成修改时间

--- pages/sleepCounter/sleepCounter.js
+++ pages/sleepCounter/sleepCounter.js
@@ -1,41 +1,48 @@
 // pages/sleepCounter/sleepCounter.js
-import { DatePicker } from '../../utils/datePicker';
-const params = {
-  dateArr:['今天','明天','后天']
-}
-const datePicker = new DatePicker(params);
-const dateArr = datePicker.datePicker();
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    dateArray: null,//picker-rang的值
-    dateIndex:null,//picker-value的值
-    dateVal:null,//显示的时间
+    date:'',
+    datePickerValue: ['', '', ''],
+    datePickerIsShow: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.set_date();
   },
 
-  //监听点击日期组件的事件变化
-  datePickerChange(e) {
-    let dateIndex = e.detail.value;
-    this.setData({
-      dateIndex,
-      dateVal: datePicker.toDate(dateArr.dateAll, dateIndex),
-    })
+  closePage() {
+    wx.navigateBack({
+      delta: 1
+    });
   },
 
-  set_date() {
+  showDatePicker: function (e) {
+    // this.data.datePicker.show(this);
     this.setData({
-      dateArray: dateArr.dateAll,
-      dateIndex: dateArr.currentDateArr,
-      dateVal: datePicker.toDate(dateArr.dateAll,dateArr.currentDateArr)
-    }) 
-  }
+      datePickerIsShow: true,
+    });
+  },
+
+  timePickerOnSureClick: function (e) {
+    console.log('PickerOnSureClick');
+    console.log(e);
+    this.setData({
+      date: `${e.detail.value[0]}${e.detail.value[1]}`,
+      datePickerValue: e.detail.value,
+      datePickerIsShow: false,
+    });
+  },
+
+  timePickerOnCancelClick: function (e) {
+    console.log('PickerOnCancelClick');
+    console.log(e);
+    this.setData({
+      datePickerIsShow: false,
+    });
+  },
 })
